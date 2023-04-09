a1d6b9309353312cad6b89b54756f58a2ff0d586
high_hopes_58 <modeller753@gmail.com>
2023-04-09 23:29:42 +0300

    add WinForm, WebClient, RegularExpressions, testing parsing

--- ParsingSite/Form1.Designer.cs
+++ ParsingSite/Form1.Designer.cs
@@ -28,13 +28,41 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.components = new System.ComponentModel.Container();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.textBox1.Location = new System.Drawing.Point(196, 33);
+			this.textBox1.Multiline = true;
+			this.textBox1.Name = "textBox1";
+			this.textBox1.ReadOnly = true;
+			this.textBox1.Size = new System.Drawing.Size(380, 341);
+			this.textBox1.TabIndex = 0;
+			this.textBox1.Text = "asd";
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
+			this.Controls.Add(this.textBox1);
+			this.Name = "Form1";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Text = "Parsing Site";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
 		}
 
 		#endregion
+
+		private System.Windows.Forms.TextBox textBox1;
 	}
 }
 
--- ParsingSite/Form1.cs
+++ ParsingSite/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,5 +16,28 @@ namespace ParsingSite
 		{
 			InitializeComponent();
 		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			textBox1.Text = ParsingResult();
+		}
+
+		private string ParsingResult()
+		{
+
+			WebClient client = new WebClient();
+			// Set user agent and also accept-encoding headers.
+			client.Headers["User-Agent"] = "test_bot";
+
+			string value = client.DownloadString("site");
+			//<td class="td-price"><p class="p-item"><span style="float:right; margin-right:10px">800 грн</span></p></td>
+			//< td class='td-price'><p class='p-item'><span style = 'float:right; margin-right:10px' > ([0 - 9]) +\S
+			string parsing_value = System.Text.RegularExpressions.Regex.Match(value, @"<td class='td-price'><p class='p-item'><span style='float:right; margin-right:10px'>([0-9]+\S)").Groups[1].Value;
+			
+			return "Parsing info = " + parsing_value + "\n";
+
+		}
+
+		
 	}
 }
